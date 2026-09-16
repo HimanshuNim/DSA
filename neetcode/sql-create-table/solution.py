895fe0699cb7badf55e05d2d36ae74d6df0819e6
Himanshu <himanshunimrad@gmail.com>
2026-09-16 17:42:54 +0530

    Add NeetCode Create Table

---
+++ neetcode/sql-create-table/solution.py
@@ -0,0 +1,11 @@
+Create table videos (
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+    id int,
+);
+WHERE table_name = 'videos';
+    name TEXT,
+    created_at date,
+    published boolean
