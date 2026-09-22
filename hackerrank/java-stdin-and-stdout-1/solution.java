1091655dd8728e3000558ae3d9b02dd1afec80e3
Himanshu <himanshunimrad@gmail.com>
2026-09-22 19:46:53 +0530

    Add HackerRank Java Stdin and Stdout I

---
+++ hackerrank/java-stdin-and-stdout-1/solution.java
@@ -0,0 +1,9 @@
+        // Complete this line
+        scan.close();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
